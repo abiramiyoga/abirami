f91fae4b9b1c6573903dee45f0c8fd9fc988b483
abiramiyoga <abiramis84@gmail.com>
2017-06-29 14:54:49 +0530

    Update swapoddevenchar.c

--- swapoddevenchar.c
+++ swapoddevenchar.c
@@ -3,17 +3,19 @@
 #include<string.h>
 void main()
 {
-int i,j;
-char str[15],temp;
+int i,sle;
+char str[25],temp;
 printf("Enter the string");
 scanf("%s",str);
-printf("The given string is %s",str);
+sle=strlen(str);
 for(i=0;i<strlen(str);i=i+2)
 {
 temp=str[i];
-str[i]=str[i+1];
+str[i+1]=str[i];
 str[i+1]=temp;
 }
-printf("after swap string",str);
+  printf("After swapping :%s:,str);
+  if(sle%2!=0)
+printf("%c",str[i-1]);
 getch();
 }
